c954c36ce9d6663edfa7affc815445d50364a29d
BrunoELuy <brunoluy.e10@gmail.com>
2025-05-21 19:56:52 -0300

    Dia 1 - Criação da rota de acesso de familiar por id único

--- app.py
+++ app.py
@@ -18,6 +18,35 @@ def get_familia():
     return lista
 
 
+def get_familiar(id):
+    con = sqlite3.connect("familia.db")
+    cur = con.cursor()
+    cur.execute(
+        "SELECT id, nome, idade, parentesco, hobbies, emprego FROM familiares WHERE id = ?", (
+            id,)
+    )
+    dado = cur.fetchone()
+    con.close()
+
+    if dado:
+        familiar = {
+            "id": dado[0],
+            "nome": dado[1],
+            "idade": dado[2],
+            "parentesco": dado[3],
+            "hobbies": dado[4],
+            "emprego": dado[5]
+        }
+        return familiar
+    else:
+        return {"erro": "Familiar não encontrado"}, 404
+
+
+@app.route('/familia/<int:id>', methods=['GET'])
+def obter_familiar(id):
+    return jsonify(get_familiar(id))
+
+
 @app.route('/familia', methods=['GET'])
 def listar_familia():
     return jsonify(get_familia())
